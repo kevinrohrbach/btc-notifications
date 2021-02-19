c067dd26cd207490fa38188b8fc78fc2d5922ea4
kevinrohrbach <kevinrohrbach@gmail.com>
2021-02-19 08:57:10 +0100

    Correction of initial

--- config.py
+++
@@ -1,8 +0,0 @@
-
-"""Define global variables."""
-api_key = 'cfbf957b-6fb7-4c2b-a33a-20472668021c'
-api_url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-bot_token = '1603797727:AAEVgLIXQ__DCEKk9R1Lu411_4M5QlYsfM0'
-chat_id = '281800755'
-threshold = 3000
-time_interval = 1 * 60  # in seconds
